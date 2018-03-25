abbdcadf09dec50eb1995329b5ce58bf98cd1a05
Rosario Alessandro Cali <racali@myseneca.ca>
2018-03-25 19:07:30 -0400

    Open Rear Camera using OpenCV and apply a Canny filter

--- FilterMe/app/src/main/java/com/group3/filterme/MainActivity.java
+++ FilterMe/app/src/main/java/com/group3/filterme/MainActivity.java
@@ -1,17 +1,37 @@
 package com.group3.filterme;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.SurfaceView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    public static final String TAG = "Main Activity";
+    private static  String TAG = "MainActivity";
+    JavaCameraView mJavaCameraView;
+    Mat mRgba, imgGrey, imgCanny;
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:
+                    mJavaCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -23,25 +43,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        // Bind variable with layout
+        mJavaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
+        // Make camera visible
+        mJavaCameraView.setVisibility(SurfaceView.VISIBLE);
+        // Set a listener for the camera
+        mJavaCameraView.setCvCameraViewListener(this);
 
-        if (OpenCVLoader.initDebug()) {
-            Context context = getApplicationContext();
-            CharSequence text = "OpenCV Works";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "OpenCV DOes not Works";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
     }
 
     /**
@@ -49,4 +57,60 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mJavaCameraView != null) {
+            mJavaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mJavaCameraView != null) {
+            mJavaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCV loaded successfully!");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        else {
+            Log.i(TAG, "OpenCV NOT loaded successfully!");
+            // Try to load OpenCV again in case of failure
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallBack);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        imgGrey = new Mat(height, width, CvType.CV_8UC1);
+        imgCanny = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+
+        // Some Basic Image Processing
+        Imgproc.cvtColor(mRgba, imgGrey, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.Canny(imgGrey, imgCanny, 50, 150);
+
+        return imgCanny;
+    }
 }
