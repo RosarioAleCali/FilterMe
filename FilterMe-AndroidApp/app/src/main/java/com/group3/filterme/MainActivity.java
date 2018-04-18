b8baa273b7bf907abe0a260dbeef4a7997a25d91
Rosario Alessandro Cali <racali@myseneca.ca>
2018-04-18 03:17:56 -0400

    Android App almost finished: need to find a solution to a 'Java Limitation'

--- FilterMe-AndroidApp/app/src/main/java/com/group3/filterme/MainActivity.java
+++ FilterMe-AndroidApp/app/src/main/java/com/group3/filterme/MainActivity.java
@@ -1,7 +1,11 @@
 package com.group3.filterme;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,13 +20,28 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Vector;
 
 import static org.opencv.android.CameraBridgeViewBase.CAMERA_ID_FRONT;
 
@@ -32,15 +51,24 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private static String successMessage = "Photo Taken ;)";
     private static String errorMessage = "Error saving photo :(";
     JavaCameraView mJavaCameraView;
-    Mat mRgba, imgGrey, imgCanny;
+    Mat mRgba, mask;
+    CascadeClassifier faceRec;
     ImageButton cameraButton, galleryButton;
     Boolean success = false;
-    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+    double minFaceSize = 20.0;
+    double maxFaceSize = 200.0;
+    private BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case BaseLoaderCallback.SUCCESS:
+                /*case BaseLoaderCallback.SUCCESS:
                     mJavaCameraView.enableView();
+                    loadCascade();
+                    break;*/
+                case LoaderCallbackInterface.SUCCESS:
+                    mJavaCameraView.enableView();
+                    mask = new Mat();
+                    loadCascade();
                     break;
                 default:
                     super.onManagerConnected(status);
@@ -109,6 +137,22 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         });
     }
 
+    private void loadMask() {
+        InputStream stream = null;
+        Uri uri = Uri.parse("android.resource://com.group3.filterme/drawable/the_mask");
+        try {
+            stream = getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+        bmpFactoryOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        Bitmap bmp = BitmapFactory.decodeStream(stream, null, bmpFactoryOptions);
+        Utils.bitmapToMat(bmp, mask);
+    }
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
@@ -151,8 +195,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat(height, width, CvType.CV_8UC4);
-        imgGrey = new Mat(height, width, CvType.CV_8UC1);
-        imgCanny = new Mat(height, width, CvType.CV_8UC1);
     }
 
     @Override
@@ -163,7 +205,143 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
-
+        findFaces();
         return mRgba;
+    }
+
+    public void loadCascade() {
+        try {
+            InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_alt2.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            faceRec = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if (faceRec.empty()) {
+                Log.d(TAG, "Error loading FaceCascade");
+                return;
+            }
+            else {
+                Log.d(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            Log.d(TAG, "Failed to load cascade. Exception thrown: " + e);
+        }
+    }
+
+    // Function used to detect faces
+    public void findFaces() {
+        // Load Mask
+        loadMask();
+
+        // Find Faces
+        MatOfRect faces = new MatOfRect();
+        Mat grey = new Mat();
+        Imgproc.cvtColor(mRgba, grey, Imgproc.COLOR_RGB2BGR);
+        faceRec.detectMultiScale(grey, faces, 1.2, 2, 0|2, new Size(0, 0), new Size(400, 400));
+
+        List<Rect> faces_ =  faces.toList();
+
+        // draw circles on the detected faces
+        if (!faces_.isEmpty()) {
+            for (int i = 0; i < faces_.size(); i++) {
+                minFaceSize = faces_.get(i).width * 0.7;
+                maxFaceSize = faces_.get(i).height * 1.5;
+                Point centre = new Point(faces_.get(i).x + faces_.get(i).width * 0.5, faces_.get(i).y + faces_.get(i).height * 0.55);
+
+//                // START JUNK
+//                Imgproc.rectangle(mRgba, new Point(centre.x - faces_.get(i).width / 2, centre.y - faces_.get(i).width / 2),
+//                        new Point(centre.x + faces_.get(i).width / 2, centre.y + faces_.get(i).width / 2), new Scalar(0,0,0));
+//                // Create Path + Filename
+//                String filename = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DCIM + "/Camera/file.jpg";
+//
+//                // Save Image
+//                success = Imgcodecs.imwrite(filename, mRgba);
+//                // END JUNK
+
+                filterMe(centre, new Size(faces_.get(i).width, faces_.get(i).height));
+            }
+        }
+    }
+
+    public void filterMe(Point centre, Size faceSize) {
+        Mat mask1 = new Mat();
+        Mat mRgba_ = new Mat();
+        Imgproc.resize(mask, mask1, faceSize);
+
+        // ROI selection
+        Rect roi = new Rect((int)(centre.x - faceSize.width / 2), (int)(centre.y - faceSize.width / 2), (int)(faceSize.width), (int)(faceSize.height));
+        // Copy sub-rectangle to new mat
+        mRgba.submat(roi).copyTo(mRgba_);
+
+        // make white region of mask transparent
+        Mat mask2 = new Mat();
+        Mat m1 = new Mat();
+        Mat m2 = new Mat();
+        Imgproc.cvtColor(mask1, mask2, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.threshold(mask2, mask2, 230, 255, Imgproc.THRESH_BINARY_INV);
+
+        //START JUNK
+        // Create Path + Filename
+        //String filename = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DCIM + "/Camera/file.jpg";
+
+        // Save Image
+        //success = Imgcodecs.imwrite(filename, mask2);
+        // END JUNK
+
+        Vector<Mat> maskChannels = new Vector<>(3);
+        Vector<Mat> resultMask = new Vector<>(3);
+        Core.split(mask1, maskChannels);
+        for (int i = 0; i < 3; i++) {
+            Mat tmp = new Mat();
+            Core.bitwise_and(maskChannels.get(i), mask2, tmp);
+            resultMask.add(tmp);
+        }
+        Core.merge(resultMask, m1);
+
+        Mat whites = new Mat(mask2.rows(), mask2.cols(), mask2.type(), new Scalar(255, 255, 255));
+        Core.subtract(whites, mask2, mask2);
+
+        Vector<Mat> srcChannels = new Vector<>(3);
+        Core.split(mRgba_, srcChannels);
+        for (int i = 0; i < 3; i++) {
+            Core.bitwise_and(srcChannels.get(i), mask2, resultMask.get(i));
+        }
+        Core.merge(resultMask, m2);
+
+        Core.addWeighted(m1, 1, m2, 1, 0, m2);
+
+        int startX = (int)(centre.x - (faceSize.width / 2));
+        int startY = (int)(centre.y - (faceSize.width / 2));
+        //byte[] pixel = new byte[3];
+
+        // Copy sub-rectangle to new mat
+        /*for (int row = 0; row < m2.rows(); row++) {
+            for (int col = 0; row < m2.rows(); row++) {
+                m2.get(row, col,pixel);
+                mRgba.put(startX + row, startY + col, pixel);
+            }
+        }*/
+        //m2.copyTo(mRgba.submat(roi));
+        //.copyTo(mRgba.submat(roi));
+
+        //START JUNK
+        // Create Path + Filename
+        //String filename = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DCIM + "/Camera/file.jpg";
+
+        // Save Image
+        //success = Imgcodecs.imwrite(filename, m2);
+        // END JUNK
     }
 }
